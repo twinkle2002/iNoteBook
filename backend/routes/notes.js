5e757eb6647b09decf951299ef85d54f02e511a8
Twinkle <twinktwinkle20@gmail.com>
2021-09-05 17:51:48 +0530

    changes done in backend

---
+++ backend/routes/notes.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const router = express.Router();
+var fetchuser = require("../middleware/fetchuser");
+const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
+
+// ROUTE1: Get All the Notes using: GET "/api/notes/getuser". Login required
+router.get('/fetchallnotes', fetchuser, async (req, res)=>{
+    try {
+        
+    const notes = await Notes.find({user: req.user.id});
+    res.json(notes)
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send("Some Error Occured");   
+}
+})
+
+// ROUTE2: Add a new Note using: POST "/api/notes/addnote". Login required
+router.post('/addnote', fetchuser, [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Description must be atleast 5 character").isLength({
+      min: 5}),
+], async (req, res)=>{
+    try {
+        
+    const {title, description, tag} = req.body;
+    // If there errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const note = new Note({
+        title, description, tag, user: req.user.id
+    })
+    const saveNote = await note.save()
+    res.json(saveNote)
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send("Some Error Occured");   
+}
+})
+
+
+// ROUTE3: Update an exsisting Note using: PUT "/api/notes/updatenote". Login required
+
+router.put('/updatenote/:id', fetchuser, async (req, res)=>{
+    const {title, description, tag} = req.body;
+
+    try {
+    // Create a newNote object
+    const newNote = {};
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+
+    // Find the note to be updated and update it
+
+    let note = await Note.findById(req.params.id);
+    if(!note){return res.status(404).send("Not Found")}
+
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+    res.json({note});
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send("Some Error Occured");   
+}
+})
+
+
+// ROUTE4: Delete an exsisting Note using: DELETE "/api/notes/deletenote". Login required
+
+router.delete('/deletenote/:id', fetchuser, async (req, res)=>{
+    try {
+
+    // Find the note to be delete and delete it
+
+    let note = await Note.findById(req.params.id);
+    if(!note){return res.status(404).send("Not Found")}
+
+    // Allowed deletion onlyif user owns this notes
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndDelete(req.params.id)
+    res.json({"Success": "Note has been deleted", note: note});
+
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send("Some Error Occured");   
+}
+})
+
+module.exports = router
